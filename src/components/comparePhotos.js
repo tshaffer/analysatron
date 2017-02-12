e9369b6749ee71710abb7469463fabe4c7b528a4
Ted Shaffer <ted@brightsign.biz>
2017-02-11 16:25:08 -0800

    successfully display 2 random photos

--- src/components/comparePhotos.js
+++ src/components/comparePhotos.js
@@ -4,18 +4,57 @@ import React, { Component } from 'react';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-export default class ComparePhotos extends Component {
+class ComparePhotos extends Component {
+
+  getImagesJSX(leftPhotoUrl, rightPhotoUrl) {
+
+    return (
+      <div className="sideBySidePhotos">
+        <div className="allImages">
+          <div className="leftPanel">
+            <img className="leftImage"
+                 src={leftPhotoUrl}
+            />
+          </div>
+          <div className="rightPanel">
+            <img className="rightImage"
+                 src={rightPhotoUrl}
+            />
+          </div>
+        </div>
+        <div className="clear" />
+      </div>
+    );
+
+  }
 
   render() {
+
+    debugger;
+
+    const imagesJSX = this.getImagesJSX(
+      this.props.googlePhotos[0].url,
+      this.props.googlePhotos[1].url
+    );
 
     return (
       <MuiThemeProvider>
         <div>
           <div>
-            <h3>Best Pizza Ever</h3>
+            {imagesJSX}
+            <div className="clear" />
           </div>
         </div>
       </MuiThemeProvider>
     );
   }
 }
+
+ComparePhotos.propTypes = {
+  googlePhotos: React.PropTypes.array.isRequired,
+  // diskImageUrl: React.PropTypes.string.isRequired,
+  // googleImageUrl: React.PropTypes.string.isRequired,
+};
+
+
+export default ComparePhotos;
