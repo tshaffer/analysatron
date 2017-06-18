0fc8c851d872b68a7bb1641413b91c336eba9a67
Ted Shaffer <shaffer.family@gmail.com>
2017-06-18 08:00:11 -0700

    working on final analysis.

--- src/store/index.js
+++ src/store/index.js
@@ -64,7 +64,11 @@ export function analyzePhotos() {
         console.log('Number of googlePhotos: ', googlePhotos.length);
 
         const rebuildGooglePhotosByHash = false;
-        getGooglePhotosByHash(rebuildGooglePhotosByHash, googlePhotos, dispatch, state);
+        getGooglePhotosByHash(rebuildGooglePhotosByHash, googlePhotos, dispatch, state).then( () => {
+          finalAnalysis(getState);
+        });
+
+      // finalAnalysis();
       }).catch( (err) => {
         console.log(err);
         debugger;
@@ -72,6 +76,151 @@ export function analyzePhotos() {
   };
 }
 
+function finalAnalysis(getState) {
+
+  // export type DrivePhotoToGooglePhotoComparisonResult = {
+  //   name: string,
+  //   path: string,
+  //   result: string
+  // }
+  //
+  // investigate
+  //      "C:\Users\Ted\Documents\RemovableMedia\8-13-05\My Photos In Process\2005\03_March_3\_DSC2477.jpg"
+  //        "hash": "1000101110101101011110011010010001100011000111000010010000000000",
+  //        "dimensions": {
+  //          "height": 2400,
+  //          "width": 1940,
+  //          "type": "jpg"
+  //         },
+  //        "lastModified": "2017-01-25T00:41:36.879Z",
+  //        "lastModifiedISO": "2017-01-25T00:41:36.879Z"
+  // no google photo match
+
+  //      "C:\Users\Ted\Documents\RemovableMedia\8-13-05\My Photos In Process\2005\03_March_3\_DSC2467.jpg"
+  // no google photo match
+
+  //  C:\Users\Ted\Documents\RemovableMedia\8-13-05\My Photos In Process\2005\03_March_3
+  // copy files
+  //    fs.createReadStream('test.log').pipe(fs.createWriteStream('newLog.log'));
+  //
+  // drivePhotosByHash
+  //      for any given hash, there can be multiple drive photos
+  //      only want to back up one of the photos, as they are identical
+  //      given a 'notAMatch' entry in drivePhotoToGooglePhotoComparisonResults, how to get the corresponding
+  //      entry in drivePhotosByHash
+  //
+  //  iterate through drivePhotos
+  //  for each drivePhoto, find corresponding record in drivePhotoToGooglePhotoComparisonResults
+  //  if notAMatch
+  //      get its hash
+  //      add a record to notAMatchByHash
+  //  iterate through notAMatchByHash
+  //    retrieve drivePhotoByHash
+  //    copy to analysatron
+
+  debugger;
+
+  let state = getState();
+  const drivePhotoToGooglePhotoComparisonResults = state.photoComparisonResults.drivePhotoToGooglePhotoComparisonResults;
+
+  let notAMatchByHash : any = {};
+  state.drivePhotos.drivePhotos.forEach( (drivePhoto) => {
+    const drivePhotoPath = drivePhoto.getPath();
+    const photoComparisonResults = drivePhotoToGooglePhotoComparisonResults[drivePhotoPath];
+    if (!photoComparisonResults) {
+      debugger;
+    }
+    if (photoComparisonResults.result === 'notAMatch') {
+      const aspectRatio = (Number(drivePhoto.getWidth()) / Number(drivePhoto.getHeight())).toString();
+      const key = drivePhoto.hash + '-' + aspectRatio;
+      const photosByHash : PhotosByHash = state.drivePhotos.drivePhotosByHash[key];
+      if (!photosByHash) {
+        debugger;
+      }
+      if (!notAMatchByHash[key]) {
+        notAMatchByHash[key] = photosByHash;
+      }
+    }
+  });
+
+  let filesToCopyByName : any = {};
+
+  for (let hash in notAMatchByHash) {
+    if (notAMatchByHash.hasOwnProperty(hash)) {
+      const photosByHash = notAMatchByHash[hash];
+      if (photosByHash.photoItems.length > 0) {
+        const photoToCopy = photosByHash.photoItems[0].photo;
+        let fileName = photoToCopy.name;
+        if (filesToCopyByName[fileName]) {
+          // yes, there are in fact, filesToCopy with the same name - need to have a unique file name
+          // append a suffix to it
+          let suffixCounter = 0;
+          while (true) {
+            fileName = photoToCopy.name + '-' + suffixCounter.toString();
+            if (!filesToCopyByName[fileName]) {
+              break;
+            }
+            suffixCounter++;
+          }
+          console.log('updated fileName: ', fileName);
+        }
+        filesToCopyByName[fileName] = photoToCopy;
+      }
+      else {
+        console.log('poo');
+      }
+    }
+  }
+  debugger;
+
+  // let resultsByPath : any = {};
+  // let resultsByName : any = {};
+  // for (let drivePhotoPath in drivePhotoToGooglePhotoComparisonResults) {
+  //   if (drivePhotoToGooglePhotoComparisonResults.hasOwnProperty(drivePhotoPath)) {
+  //     let photoComparisonResult: DrivePhotoToGooglePhotoComparisonResult = drivePhotoToGooglePhotoComparisonResults[drivePhotoPath];
+  //     if (photoComparisonResult.result === 'notAMatch') {
+  //       let fullPath = photoComparisonResult.path;
+  //       let dirname = path.dirname(fullPath);
+  //       if (!resultsByPath[dirname]) {
+  //         let empty  : Array<DrivePhotoToGooglePhotoComparisonResult> = [];
+  //         resultsByPath[dirname] = empty;
+  //       }
+  //       let existingResults : Array<DrivePhotoToGooglePhotoComparisonResult> = resultsByPath[dirname];
+  //       existingResults.push(photoComparisonResult);
+  //
+  //       if (resultsByName[photoComparisonResult.name]){
+  //         debugger;
+  //       }
+  //       resultsByName[photoComparisonResult.name] = fullPath;
+  //     }
+  //   }
+  // }
+}
+
+function copyFile(source, target, cb) {
+  let cbCalled = false;
+
+  let rd = fs.createReadStream(source);
+  rd.on("error", function(err) {
+    done(err);
+  });
+  let wr = fs.createWriteStream(target);
+  wr.on("error", function(err) {
+    done(err);
+  });
+  wr.on("close", function(ex) {
+    done();
+  });
+  rd.pipe(wr);
+
+  function done(err) {
+    if (!cbCalled) {
+      cb(err);
+      cbCalled = true;
+      debugger;
+    }
+  }
+}
 /*
 Results:
     Number of googlePhotos:  7773
@@ -95,102 +244,98 @@ Results:
 
 function getGooglePhotosByHash(rebuildGooglePhotosByHash: boolean, googlePhotos, dispatch, state) {
 
-  let googlePhotosByHash : PhotosByHash = {};
+  return new Promise((resolve) => {
+    let googlePhotosByHash : PhotosByHash = {};
 
-  if (rebuildGooglePhotosByHash) {
-    googlePhotosByHash = getMatchingPhotos(googlePhotos);
-    getDrivePhotos(false, googlePhotos, googlePhotosByHash, dispatch, state);
-  }
-  else {
-    readFile('googlePhotosByHash.json').then((googlePhotosByHashBuffer) => {
-      let googlePhotosByHashStr = decoder.write(googlePhotosByHashBuffer);
-      let googlePhotosByHashRaw = JSON.parse(googlePhotosByHashStr);
+    if (rebuildGooglePhotosByHash) {
+      googlePhotosByHash = getMatchingPhotos(googlePhotos);
+      getDrivePhotos(false, googlePhotos, googlePhotosByHash, dispatch, state).then( () => {
+        resolve();
+      });
+    }
+    else {
+      readFile('googlePhotosByHash.json').then((googlePhotosByHashBuffer) => {
+        let googlePhotosByHashStr = decoder.write(googlePhotosByHashBuffer);
+        let googlePhotosByHashRaw = JSON.parse(googlePhotosByHashStr);
 
-      for (let hash in googlePhotosByHashRaw) {
-        if (googlePhotosByHashRaw.hasOwnProperty(hash)) {
-          const identicalPhotos = googlePhotosByHashRaw[hash];
-          const photoItems = identicalPhotos.photoItems;
-          photoItems.forEach((photoItem) => {
-            const photo = new GooglePhoto(
-              {
-                name: photoItem.photo.name,
-                hash: photoItem.photo.hash,
-                url: photoItem.photo.url,
-                width: photoItem.photo.width,
-                height: photoItem.photo.height,
-                dateTime: photoItem.photo.dateTime,
-                exifDateTime: photoItem.photo.exifDateTime
-              }
-            );
-            photoItem.photo = photo;
-          });
+        for (let hash in googlePhotosByHashRaw) {
+          if (googlePhotosByHashRaw.hasOwnProperty(hash)) {
+            const identicalPhotos = googlePhotosByHashRaw[hash];
+            const photoItems = identicalPhotos.photoItems;
+            photoItems.forEach((photoItem) => {
+              const photo = new GooglePhoto(
+                {
+                  name: photoItem.photo.name,
+                  hash: photoItem.photo.hash,
+                  url: photoItem.photo.url,
+                  width: photoItem.photo.width,
+                  height: photoItem.photo.height,
+                  dateTime: photoItem.photo.dateTime,
+                  exifDateTime: photoItem.photo.exifDateTime
+                }
+              );
+              photoItem.photo = photo;
+            });
 
-          googlePhotosByHash[hash] = identicalPhotos;
+            googlePhotosByHash[hash] = identicalPhotos;
+          }
         }
-      }
-      getDrivePhotos(false, googlePhotos, googlePhotosByHash, dispatch, state);
-    });
-  }
+        getDrivePhotos(false, googlePhotos, googlePhotosByHash, dispatch, state).then( () => {
+          resolve();
+        });
+      });
+    }
+  });
 }
 
 
 function getDrivePhotos(rebuildDrivePhotosByHash, googlePhotos, googlePhotosByHash, dispatch, state) {
 
-  dispatch(setGooglePhotosByHash(googlePhotosByHash));
-  console.log('Number of google photos with unique keys: ', Object.keys(googlePhotosByHash).length);
+  return new Promise( (resolve) => {
+    dispatch(setGooglePhotosByHash(googlePhotosByHash));
+    console.log('Number of google photos with unique keys: ', Object.keys(googlePhotosByHash).length);
 
-  let drivePhotosByHash : PhotosByHash = {};
+    let drivePhotosByHash : PhotosByHash = {};
 
-  const drivePhotos  = state.drivePhotos.drivePhotos;
-  console.log('Number of drivePhotos: ', drivePhotos.length);
+    const drivePhotos  = state.drivePhotos.drivePhotos;
+    console.log('Number of drivePhotos: ', drivePhotos.length);
 
-  if (rebuildDrivePhotosByHash) {
-    drivePhotosByHash = getMatchingPhotos(drivePhotos);
-    processDrivePhotosByHash(drivePhotosByHash, googlePhotos, googlePhotosByHash, dispatch);
-  }
-  else {
-    readFile('drivePhotosByHash.json').then((drivePhotosByHashBuffer) => {
-      let drivePhotosByHashStr = decoder.write(drivePhotosByHashBuffer);
-      let drivePhotosByHashRaw = JSON.parse(drivePhotosByHashStr);
-      for (let hash in drivePhotosByHashRaw) {
-        if (drivePhotosByHashRaw.hasOwnProperty(hash)) {
-
-          const identicalPhotos = drivePhotosByHashRaw[hash];
-          const photoItems = identicalPhotos.photoItems;
-          let index = 0;
-          while (index < photoItems.length) {
-            let photoItem = photoItems[index];
-            if (path.extname(photoItem.photo.path).toLowerCase() !== '.tif') {
-              photoItem.photo = new DrivePhoto(photoItem.photo);
-              index++;
-            }
-            else {
-              photoItems.splice(index, 1);
-            }
-          }
-          drivePhotosByHash[hash] = identicalPhotos;
-        }
-      }
-
-      // for (let poo in drivePhotosByHash) {
-      //   if (drivePhotosByHash.hasOwnProperty(poo)) {
-      //     let drivePhotoByHash : IdenticalPhotos = drivePhotosByHash[poo];
-      //     let photoItems = drivePhotoByHash.photoItems;
-      //     photoItems.forEach( (photoItem) => {
-      //       if (!(photoItem.photo instanceof DrivePhoto)) {
-      //         console.log(drivePhotosByHash);
-      //         console.log(poo);
-      //         console.log(drivePhotoByHash);
-      //         console.log(photoItems);
-      //         console.log(photoItem);
-      //         debugger;
-      //       }
-      //     })
-      //   }
-      // }
+    if (rebuildDrivePhotosByHash) {
+      drivePhotosByHash = getMatchingPhotos(drivePhotos);
       processDrivePhotosByHash(drivePhotosByHash, googlePhotos, googlePhotosByHash, dispatch);
-    });
-  }
+      // RESOLVE?
+    }
+    else {
+      readFile('drivePhotosByHash.json').then((drivePhotosByHashBuffer) => {
+        let drivePhotosByHashStr = decoder.write(drivePhotosByHashBuffer);
+        let drivePhotosByHashRaw = JSON.parse(drivePhotosByHashStr);
+        for (let hash in drivePhotosByHashRaw) {
+          if (drivePhotosByHashRaw.hasOwnProperty(hash)) {
+
+            const identicalPhotos = drivePhotosByHashRaw[hash];
+            const photoItems = identicalPhotos.photoItems;
+            let index = 0;
+            while (index < photoItems.length) {
+              let photoItem = photoItems[index];
+              if (path.extname(photoItem.photo.path).toLowerCase() !== '.tif') {
+                photoItem.photo = new DrivePhoto(photoItem.photo);
+                index++;
+              }
+              else {
+                photoItems.splice(index, 1);
+              }
+            }
+            drivePhotosByHash[hash] = identicalPhotos;
+          }
+        }
+
+        processDrivePhotosByHash(drivePhotosByHash, googlePhotos, googlePhotosByHash, dispatch);
+
+        resolve();
+      });
+    }
+
+  });
 }
 
 function processDrivePhotosByHash(drivePhotosByHash, googlePhotos, googlePhotosByHash, dispatch) {
